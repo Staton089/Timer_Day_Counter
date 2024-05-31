f66d68082e3e5390b36d5689dd75aed0c2c0428f
staton089 <staton089@gmail.com>
2024-05-31 16:40:56 -0400

    added code for userData for the date and also added an event listener to collect the data.

---
+++ index.js
@@ -0,0 +1,8 @@
+//Collect the Users Date 
+let userDate = document.getElementById("userDate");
+
+// Add an event listener to capture the date when it changes
+userDate.addEventListener("change", function() {
+    console.log(userDate.value);
+});
+
